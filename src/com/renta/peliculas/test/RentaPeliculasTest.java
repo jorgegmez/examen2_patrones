2c9ce46cefe3c3d20bc109dc644ca395776d92bc
Jorge Gómez <jgomezm@ucenfotec.ac.cr>
2018-04-12 12:56:44 -0600

    Initial Commit

---
+++ src/com/renta/peliculas/test/RentaPeliculasTest.java
@@ -0,0 +1,14 @@
+package com.renta.peliculas.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class RentaPeliculasTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
